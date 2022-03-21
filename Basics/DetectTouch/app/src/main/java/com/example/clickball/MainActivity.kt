fdf55500be7219138f58fa4704794e7b9fac4c2d
PierreVieira <pierrevieiraggg@gmail.com>
2022-03-20 22:42:46 -0300

    move file

--- Basics/DetectTouch/app/src/main/java/com/example/clickball/MainActivity.kt
+++ Basics/DetectTouch/app/src/main/java/com/example/clickball/MainActivity.kt
@@ -3,14 +3,26 @@ package com.example.clickball
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.clickball.ui.theme.ClickBallTheme
+import kotlinx.coroutines.delay
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,23 +33,130 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 }
 
 @Composable
 private fun MainScreen() {
-    TODO("Not yet implemented")
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ClickBallTheme {
-        Greeting("Android")
+    var points by remember {
+        mutableStateOf(0)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Points: $points",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Button(onClick = {
+                isTimerRunning = !isTimerRunning
+                points = 0
+            }) {
+                Text(text = if (isTimerRunning) "Reset" else "Start")
+            }
+            CountDownTimer(
+                isTimerRunning = isTimerRunning
+            ) {
+                isTimerRunning = false
+            }
+        }
+        BallClicker(
+            enabled = isTimerRunning
+        ) {
+            points++
+        }
     }
 }
+
+@Composable
+fun CountDownTimer(
+    time: Int = 30_000,
+    isTimerRunning: Boolean = false,
+    onTimerEnd: () -> Unit = {},
+) {
+    var curTime by remember {
+        mutableStateOf(time)
+    }
+    LaunchedEffect(key1 = curTime, key2 = isTimerRunning) {
+        if (!isTimerRunning) {
+            curTime = time
+            return@LaunchedEffect
+        }
+        if (curTime > 0) {
+            delay(1000L)
+            curTime -= 1000
+        } else {
+            onTimerEnd()
+        }
+    }
+    Text(
+        text = "${curTime / 1000}",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun BallClicker(
+    radius: Float = 100f,
+    enabled: Boolean = false,
+    ballColor: Color = Color.Red,
+    onBallClick: () -> Unit = {},
+) {
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        var ballPosition by remember {
+            mutableStateOf(
+                randomOffset(
+                    radius = radius,
+                    width = constraints.maxWidth,
+                    height = constraints.maxHeight
+                )
+            )
+        }
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(enabled) {
+                    if (!enabled) {
+                        return@pointerInput
+                    }
+                    detectTapGestures {
+                        val distance = sqrt((it.x - ballPosition.x).pow(2) +
+                                (it.y - ballPosition.y).pow(2))
+                        if (distance <= radius) {
+                            ballPosition = randomOffset(
+                                radius = radius,
+                                width = constraints.maxWidth,
+                                height = constraints.maxHeight
+                            )
+                            onBallClick()
+                        }
+                    }
+                }
+        ) {
+            drawCircle(
+                color = ballColor,
+                radius = radius,
+                center = ballPosition
+            )
+        }
+    }
+}
+
+private fun randomOffset(radius: Float, width: Int, height: Int): Offset = Offset(
+    x = Random.nextInt(
+        from = radius.toInt(),
+        until = width - radius.roundToInt()
+    ).toFloat(),
+    y = Random.nextInt(
+        from = radius.roundToInt(),
+        until = height - radius.toInt()
+    ).toFloat()
+)
